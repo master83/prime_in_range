31d13cdde18f470e714d33f41d936ac6b0b926dd
Rahul Kumar <rforrahul83@gmail.com>
2020-07-16 12:16:04 -0400

    With list comprehension

--- prime.py
+++ prime.py
@@ -4,17 +4,9 @@ def run():
 
 
 def calculate_prime_in_range(max_range):
-    prime_numbers = []
     if max_range <= 1:
         return []
-    for num in range(2, max_range):
-        is_prime = True
-        for i in range(2, num):
-            if num % i == 0:
-                is_prime = False
-        if is_prime:
-            prime_numbers.append(num)
-    return prime_numbers
+    return [n for n in range(2, max_range) if all(n % i != 0 for i in range(2, n))]
 
 
 if __name__ == '__main__':
